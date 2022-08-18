5d8fc388133edec612c7b77133199a017160054a
Pushkar Srivastava <d.errorfound@gmail.com>
2022-08-19 03:00:44 +0530

    bug fix

--- lib/flutter_pkg.dart
+++ lib/flutter_pkg.dart
@@ -4,5 +4,5 @@ library flutter_pkg;
 class Calculator {
   /// Returns [value] plus 1.
   int addOne(int value) => value + 1;
-  String title ="Git repo from flutter update version 3";
+  String title ="Git repo from flutter update version 4";
 }
